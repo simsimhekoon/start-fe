d4c9799929c100f33c9e69e43e005e65ba2ece81
simsimhekoon <37258541+simsimhekoon@users.noreply.github.com>
2020-03-26 17:54:17 +0900

    1회차 과제
    1회차 과제입니다.

---
+++ 01/app.js
@@ -0,0 +1,81 @@
+var age = 20;
+
+var TEST = true;
+
+var month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, 12];
+
+var profile = {
+    name: "gildong",
+    age: 25
+};
+
+
+
+
+
+function log(str){
+    console.log(str);
+}
+log(age);
+log(TEST);
+log(month);
+log(profile);
+
+
+
+/* 아래는 실습내용입니다.
+console.log('app.js');
+
+var name = "Test";
+var Name = "Test";
+
+console.log(name);
+console.log(Name);
+
+//주석
+
+
+var count = 1;
+console.log(count);
+
+var TEST = true;
+console.log(TEST);
+
+var TEST2 = true;
+console.log(TEST2, true);
+
+function log(str){
+    console.log(str);
+}
+
+
+log('hello');
+
+var log2 = function(str) {
+    console.log(str);
+};
+log2('hello2');
+
+
+var image = {};
+console.log(image);
+
+
+
+var profile = {
+    name: "gildong",
+    age: 25
+};
+
+log(profile);
+log(profile.name);
+log(profile.age);
+
+var month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, 12];
+log(month);
+log(month[3]);
+
+console.log(1 + 2);
+console.log('a' + 'b');
+console.log(1 + '4');
+*/
