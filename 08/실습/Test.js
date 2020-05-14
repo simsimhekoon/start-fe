712123e40712eaa00f0b3923a121336a17fb52e4
simsimhekoon <37258541+simsimhekoon@users.noreply.github.com>
2020-05-14 16:08:52 +0900

    0514-실습

---
+++ 08/실습/Test.js
@@ -0,0 +1,15 @@
+// const promise = fetch('./json.json');
+
+// console.log(promise);
+
+// promise.then((res) => {
+//   console.log('res:', res);
+//   const textPromise = res.json();
+//   console.log('textPromise: ', textPromise);
+
+//   textPromise.then((data) => console.log('data:', data));
+// });
+
+function callback(data) {
+  console.log('callback: ', data);
+}
