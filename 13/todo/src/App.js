3ba06a05bd3a994711a4ddd8da722e491ed1bd19
simsimhekoon <37258541+simsimhekoon@users.noreply.github.com>
2020-06-18 21:36:47 +0900

    count

--- 13/todo/src/App.js
+++ 13/todo/src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './Header';
 
 function App() {
-  return <div className="App">Hello! World!!</div>;
+  const [countNumber, setCountNumver] = useState(0);
+
+  function addCountNumber() {
+    setCountNumver(countNumber + 1);
+  }
+
+  return (
+    <div className="App">
+      <Header />
+      <div>{countNumber}</div>
+      <button onClick={addCountNumber}>플러스</button>
+    </div>
+  );
 }
 
 export default App;
